7b1a03d9ce5b4bef9ac99784c78f1f882ecbeb91
chlipounni <50593554+chlipounni@users.noreply.github.com>
2021-04-14 15:06:41 +0200

    fonction ok

--- web/client .js
+++ web/client .js
@@ -1,15 +1,28 @@
 var client = new Paho.MQTT.Client("mqtt-ws.sdi.hevs.ch", 80, "/ws", "sdi17" + Math.floor(Math.random() * 100));
-boolean sp = false;
+String thingies1 = 'DF:66:32:49:C8:1A';
+string thingies2 = 'DC:06:D9:40:7A:CB';
+
+
+
 enum type {bigHead,head,body,bigBody,tail,noIdea};
-int WIDTH = 30;
-int HEIGHT = 30;
-int resolut = 20;
-int size = 3;
-int score = 0;
+
 public pos {
   int x = 0;
   int Y = 0;
 }
+
+//param game
+{
+int WIDTH = 300;
+int HEIGHT = 300;
+int resolut = 10;
+int size = 3;
+int score = 0;
+int timeM = 500; // 2 deplacement pour 1s
+//int timeD = 40; //non utiliser pour le momant, par de rafraichisement continue
+boolean sp = false;
+}
+
 public warmBody {
   pos p;
   type part = noIdea;
@@ -18,9 +31,29 @@ pos vHead;
 vector <warmBody> warm = new vector <warmBody>;
 vector <pos> miam = new vector <pos>;
 
-
-client.onConnectionLost = function(responseObject)
+//image game
 {
+var bigHead = new Image();
+var head = new Image();
+var bigBody = new Image();
+var body = new Image();
+var tail = new Image();
+var miam = new Image();		 
+var background = new Image();
+var gameOver = new Image();
+}
+
+//canvas
+var gw = document.getElementById('GameWindow');
+var ctx = gw.getContext('2d');
+gw.width = WIDTH;
+gw.height = HEIGHT;
+
+preload();
+drawGame();
+
+
+client.onConnectionLost = function(responseObject){
   if (responseObject.errorCode !== 0)
   {
     console.error("Lost connection:" + responseObject.errorMessage);
@@ -31,8 +64,7 @@ client.onConnectionLost = function(responseObject)
   }
 }
 
-client.onMessageArrived = function(message)
-{
+client.onMessageArrived = function(message){
   var logTA = document.getElementById('log');
   var splittedName = message.destinationName.split('/');
   
@@ -44,11 +76,15 @@ client.onMessageArrived = function(message)
                         + obj.connected=='true'?'Connected':'Disconnected' + '\n'
                         + 'Thingies connected: ' + obj.thingies.length + '\n';
       break;
-    case 'DF:66:32:49:C8:1A':
-      logTA.textContent += 'Button of Thingy 1: ' + ((message.payloadString=="true")?'pressed':'released') + '\n';
-      break;
-    case 'DC:06:D9:40:7A:CB':
-      logTA.textContent += 'Button of Thingy 2: ' + ((message.payloadString=="true")?'pressed':'released') + '\n';
+    case thingies1:
+		if(message.payloadString=="true"){
+			turnL();
+		}
+	  break;
+    case thingies2:
+		if(message.payloadString=="true"){
+			turnR();
+		}
       break;
     default:
       console.log("Got message: topic=" + message.destinationName + ', payload=' + message.payloadString);
@@ -65,12 +101,12 @@ client.connect({
     console.log("Connected.");
     client.subscribe("sdi17/status");
     client.subscribe("sdi17/+/button");
-    client.send('sdi17/DF:66:32:49:C8:1A/led', JSON.stringify({
+    client.send('sdi17/'+ thingies1+'/led', JSON.stringify({
       red: 0,
       green: 255,
       blue: 0
     }));
-    client.send('sdi12/DC:06:D9:40:7A:CB/led', JSON.stringify({
+    client.send('sdi12/'+thingies2+'/led', JSON.stringify({
       red: 255,
       green: 0,
       blue: 0
@@ -81,7 +117,7 @@ client.connect({
   }
 });
 
-function setLED(n,red,green,blue){
+function void setLED(n,red,green,blue){
   if(n==1)
   {
     client.send('sdi17/DF:66:32:49:C8:1A/led', '{"red": ' + red + ',"green": ' + green + ',"blue": ' + blue + '}');
@@ -98,21 +134,24 @@ function void startStop(){
   sp = false;
   setLED(1,0,0,255);
   setLED(2,0,0,255);
-  //stoptimer;
+  //stopTimer
+  
 	}else{
 	//restart game
   sp = true;
   setLED(1,255,0,0);
   setLED(2,0,255,0);
   //starttimer;
+  runGame();
 	}
 }
 
 function void reStart(){
   score = 0;
   size = 3;
+  timeM = 500;
   
-  sp = true;
+  sp = true;	//pour passer en Mode pause apres
   startStop();
   
   warm.clean;
@@ -137,44 +176,53 @@ function void reStart(){
   vHead.x= resolut;
   vHead.y=0;
   
-  //draw
+  miam.clean;
+  addMiam();
+  
+  drawGame();
 }
 
 function void runGame(){
-  move();
-  //draw
-  
+  if(sp == true){
+	setTimeOut(runGame(),time);
+	move();
+  }
+  drawGame();
 }
 
-function bool checkCollision() {
+function boolean checkCollision(){
   for (int y: miam){
-    if(((warm.get(0).px+warm.get(0).vx) ==  miam.get(y).px) && ((warm.get(0).py+warm.get(0).vy) == miam.get(y).py)){
+    if(((warm.get(0).p.x+vHead.x) ==  miam.get(y).x) && ((warm.get(0).p.y+vHead.y) == miam.get(y).y)){
       score = score+1;
+	  addMiam();
       warm.get(0).part=bigHead;
     }
   }
-  if(((warm.get(0).px+warm.get(0).vx) || (warm.get(0).py+warm.get(0).vy))<= 0){
+  
+  if(((warm.get(0).px + vHead.x) || (warm.get(0).py + vHead.y))<= 0){
     retun true;
   }
-  else if(((warm.get(0).px+warm.get(0).vx) >= WIDTH){
+  else if((warm.get(0).px+vHead.x) >= WIDTH){
     retun true;
   }
-  else if(((warm.get(0).py+warm.get(0).vy) >= HEIGHT){
+  
+  else if((warm.get(0).py+vHead.y) >= HEIGHT){
     retun true;
-  } 
+  }
   
   for(int x =1; x<= warmBody.size()-1;x++){
-    if(((warm.get(0).px+warm.get(0).vx) ==  warm.get(x).px) && ((warm.get(0).py+warm.get(0).vy) == warm.get(y).py)){
+    if(((warm.get(0).p.x+vHead.x) ==  warm.get(x).p.x) && ((warm.get(0).p.y+vHead.y) == warm.get(y).p.y)){
       retun true;
     }
   }
   retun false;
 }
 
-function move(){
+function void move(){
   if(checkCollision()){
       sp= true;
-      game over;
+      //game over;
+	  drawGameOver();
   }else{
     //tail
     if(warm.get(size-2).part == bigBody){
@@ -204,4 +252,109 @@ function move(){
     warm.get(0).p.x = warm.get(0).p.x + vHead.x;
     warm.get(0).p.y = warm.get(0).p.y + vHead.y; 
   }
+}
+
+function void turnL{
+	if(vHead.x == resolut){
+		vHead.x = 0;
+		vHead.y = -resolut;
+	}else if(vHead.y == -resolut){
+		vHead.y = 0;
+		vHead.x = -resolut;
+	}else if(vHead.x == -resolut){
+		vHead.x = 0;
+		vHead.y = resolut;
+	}else if(vHead.y == resolut){
+		vHead.y = 0;
+		vHead.x = resolut;
+	}	
+}
+
+function void turnR{
+	if(vHead.x == resolut){
+		vHead.x = 0;
+		vHead.y = resolut;
+	}else if(vHead.y == resolut){
+		vHead.y = 0;
+		vHead.x = -resolut;
+	}else if(vHead.x == -resolut){
+		vHead.x = 0;
+		vHead.y = -resolut;
+	}else if(vHead.y == -resolut){
+		vHead.y = 0;
+		vHead.x = resolut;
+	}
+}
+
+function void addMiam(){
+	int x = 0;
+	int y = 0;
+	boolean ok = true;
+	
+	do{
+		ok = true;
+		x = Math.random() * (WIDTH/resolut);
+		y = Math.random() * (HEIGHT/resolut);
+		
+		for(int x =1; x<= warmBody.size();x++){
+			if((warm.get(x).p.x ==  x) || (warm.get(x).p.y == y)){
+				ok = false;
+			}
+		}
+		
+		for(int i =1; i <= miam.size();i++){
+			if((miam.get(i).x ==  x) || (miam.get(i).y == y)){
+				ok = false;
+			}
+		}
+	}while(!ok);
+	
+	pos m = new pos;
+	m.x = x;
+	m.y= y;
+	miam.add(m);
+}
+
+function void drawGame(){
+	for(warm wb:warmBody){
+		switch (wb.type){
+			case bigHead:
+			 ctx.drawImage(bigHead, wb.p.x, wb.p.y);
+			 break;
+			case head:
+			 ctx.drawImage(head, wb.p.x, wb.p.y);
+			 break;
+			 case bigBody:
+			 ctx.drawImage(bigBody, wb.p.x, wb.p.y);
+			 break;
+			 case body:
+			 ctx.drawImage(body, wb.p.x, wb.p.y);
+			 break;
+			 case tail:
+			 ctx.drawImage(tail, wb.p.x, wb.p.y);
+			 break;
+			 default:
+			 break;
+		}
+	}
+	for(miam m:pos){
+		ctx.drawImage(miam, m.x, m.y);
+	}
+}
+
+function void drawGameOver (){
+	ctx.drawImage(gameOver, (WIDTH/2)-gameOver.width, (HEIGHT/2)-gameOver.height);
+	
+}
+function preload() {
+	bigHead.src = '../../images/bigHead.jpg';
+	head.src = '../../images/head.jpg';
+	bigBody.src = '../../images/bigBody.jpg';
+	body.src = '../../images/body.jpg';
+	tail.src = '../../images/tail.jpg';
+	miam.src = '../../images/miam.jpg';
+	background.src = '../../images/background.jpg';
+	gameOver.src = '../../images/gameOver.jpg';
+	
+	ctx.drawImage(background, 0, 0,WIDTH,HEIGHT,null);
 }
